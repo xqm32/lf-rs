74e41d0e6029abe4b344534d654546374a726111
xqm32 <458173774@qq.com>
2022-09-14 14:06:22 +0800

    init commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::fs;
+use windows_sys::Win32::Storage::FileSystem::*;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let name = args.get(1).expect("Except an argument.");
+
+    fs::write(
+        "desktop.ini",
+        format!("[.ShellClassInfo]\nLocalizedResourceName={}\n", name),
+    )
+    .expect("Error");
+
+    unsafe {
+        SetFileAttributesA(
+            b"desktop.ini\0".as_ptr(),
+            FILE_ATTRIBUTE_ARCHIVE | FILE_ATTRIBUTE_HIDDEN | FILE_ATTRIBUTE_SYSTEM,
+        );
+    }
+}
